7db40d76b6b3d920961e0dca657469ae8130b267
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-03 16:04:52 -0500

    adding controllers and separating concerns

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-const inquirer = require('inquirer');
+const MenuController = require('./controllers/MenuController');
 
-const questions = [
-  {
-    type: "list",
-    name: "status",
-    message: "Are you hungry?: ",
-    choices: [
-      "Yes",
-      "No"
-    ]
-  }
-];
+const menu = new MenuController();
+menu.main();
 
-inquirer
-  .prompt(questions)
-  .then((answers) => (
-    answers.status === "Yes" 
-      ? console.log("Get up and eat!")
-      : console.log("Get back to work!")
-  ))
-  .catch((err) => {
-    console.error(err);
-  });
