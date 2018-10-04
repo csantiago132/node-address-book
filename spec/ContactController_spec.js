94b1e9d405569f8d39267dce8a066c68d3ceb31c
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-04 15:36:28 -0500

    tdd binarySearch

--- spec/ContactController_spec.js
+++ spec/ContactController_spec.js
@@ -64,54 +64,115 @@ describe('ContactController', () => {
     });
   });
 
-  describe("#iterativeSearch()", () => {
-
+  describe("#iterativeSearch()", () => {     
     const zelda = ["Zelda Smith", "000-100-111", "zelda@nintendo.com"];
     const snake = ["Solid Snake", "100-100-100", "snake@konami.com"];
     const magus = ["Magus Johnson", "101-010-101", "magus@squaresoft.com"];
     const alloy = ["Alloy Rodriguez", "111-111-111", "allow@guerrilla-games.com"];
 
-    it("should return null when called with an empty array", () => {
-      expect(this.book.iterativeSearch([], "Alloy")).toBeNull();
-    });
 
-    it("should return null when contact is not found", (done) => {
-      this.book.addContact(...zelda)
-      .then(() => {
-        this.book.getContacts()
-        .then((contacts) => {
-          expect(this.book.iterativeSearch(contacts, "Alloy Rodriguez")).toBeNull();
-          done();
-        })
-        .catch((err) => {
-          console.log(err);
-          done();
+    describe("#iterativeSearch()", () => {
+      it("should return null when called with an empty array", () => {
+        expect(this.book.iterativeSearch([], "Alloy")).toBeNull();
+      });
+
+      it("should return null when contact is not found", (done) => {
+        this.book.addContact(...zelda)
+        .then(() => {
+          this.book.getContacts()
+          .then((contacts) => {
+            expect(this.book.iterativeSearch(contacts, "Alloy Rodriguez")).toBeNull();
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
         });
+      });
+
+      it("should return the contact if found", (done) => {
+        const addUser1 = this.book.addContact(...alloy);
+        const addUser2 = this.book.addContact(...magus);
+        
+        Promise.all([
+          addUser1,
+          addUser2
+        ])
+        .then(() => {
+          this.book.getContacts()
+          .then((contacts) => {
+            let contact = this.book.iterativeSearch(contacts, "Magus Johnson");
+            expect(contact.name).toBe("Magus Johnson");
+            expect(contact.phone).toBe("101-010-101");
+            expect(contact.email).toBe("magus@squaresoft.com");
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        })
       });
     });
 
-    it("should return the contact if found", (done) => {
-      const addUser1 = this.book.addContact(...alloy);
-      const addUser2 = this.book.addContact(...magus);
-      
-      Promise.all([
-        addUser1,
-        addUser2
-      ])
-      .then(() => {
-        this.book.getContacts()
-        .then((contacts) => {
-          let contact = this.book.iterativeSearch(contacts, "Magus Johnson");
-          expect(contact.name).toBe("Magus Johnson");
-          expect(contact.phone).toBe("101-010-101");
-          expect(contact.email).toBe("magus@squaresoft.com");
-          done();
-        })
-        .catch((err) => {
-          console.log(err);
-          done();
+    describe("#binarySearch()", () => {
+        const sort = (contacts) => (
+          contacts.sort((a, b) => {
+            if(a.name > b.name) return 1;
+              else if(a.name < b.name) return -1;
+              else return 0;
+            })
+        )
+ 
+        it("should return null when called with an empty array", () => {
+          expect(this.book.binarySearch([], "Alloy Rodriguez")).toBeNull();
         });
-      })
+ 
+        it("should return null when contact is not found", (done) => {
+          this.book.addContact(...zelda)
+          .then(() => {
+            this.book.getContacts()
+            .then((contacts) => {
+              expect(this.book.binarySearch(sort(contacts), "Alloy Rodriguez")).toBeNull();
+              done();
+            })
+            .catch((err) => {
+              console.log(err);
+              done();
+            });
+          })
+        });
+
+        it("should return the contact if found", (done) => {
+          const promises = {
+            promise1: this.book.addContact(...alloy),
+            promise2: this.book.addContact(...magus),
+            promise3: this.book.addContact(...zelda),
+            promise4: this.book.addContact(...snake)
+          }
+          Promise.all([
+            promises.promise1,
+            promises.promise2,
+            promises.promise3,
+            promises.promise4,
+            promises.promises
+          ])
+          .then(() => {
+            this.book.getContacts()
+              .then((contacts) => {
+                let contact = this.book.binarySearch(sort(contacts), "Magus Johnson");
+                expect(contact.name).toBe("Magus Johnson");
+                expect(contact.phone).toBe("101-010-101");
+                expect(contact.email).toBe("magus@squaresoft.com");
+                done();
+              })
+              .catch((error) => {
+                console.log(error);
+                done();
+              });
+          });
+        });
     });
 
   });
